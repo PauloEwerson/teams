09d12085d12ce042175c2d3e9aada3e4386fae39
PauloEwerson <ewersonmrt@gmail.com>
2023-04-21 18:16:50 -0300

    feat: Cria um componente Loading para o ActivityIndicator

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
-import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import Groups from './src/screens/Groups';
 import theme from './src/theme';
+
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }

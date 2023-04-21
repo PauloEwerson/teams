a507897b100b9f608cf7497215e3dd4161f0aab2
PauloEwerson <ewersonmrt@gmail.com>
2023-04-21 17:13:05 -0300

    Define tipagem do tema

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles'
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <h1>Groups</h1>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

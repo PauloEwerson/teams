2a8ef6a0a88147383a021b7885973d35cdbc32f9
PauloEwerson <ewersonmrt@gmail.com>
2023-04-24 11:49:49 -0300

    feat: Adiciona componente GroupCard com propriedades de clique (TouchableOpacityProps) e um título (string), ambos tipados. O componente exibe um ícone e o título em um contêiner.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Hightlight';
+import { GroupCard } from '@components/GroupCard';
+
 import { Container } from './styles'
 
 export default function Groups() {
@@ -7,6 +9,8 @@ export default function Groups() {
     <Container>
       <Header />
       <Hightlight title='Turmas' subtitle='Jogue com sua turma'/>
+
+      <GroupCard title='Turma 1' />
     </Container>
   );
 }

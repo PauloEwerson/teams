55797bc2082b4bb5331bac8cd46ad615ad12fae5
PauloEwerson <ewersonmrt@gmail.com>
2023-05-01 18:50:03 -0300

    feat: Apresenta em tela um componente caso a listagem esteja vazia. Define o atributo ListEmptyComponent da FlatList, repassando para ela o componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,25 +1,30 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Hightlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles'
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Amigos', 'Familia']);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
-      <Hightlight title='Turmas' subtitle='Jogue com sua turma'/>
+      <Hightlight title='Turmas' subtitle='Jogue com sua turma' />
 
-      <FlatList 
+      <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Cadastre a primeira turma' />
         )}
       />
     </Container>

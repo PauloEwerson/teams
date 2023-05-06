90e6dc5b2db03aff0534708cca934dbd8d22b707
PauloEwerson <ewersonmrt@gmail.com>
2023-05-06 14:45:34 -0300

    feat: Cria as rotas da aplicação utilizando o Stack Navigator

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -8,7 +8,7 @@ import { Button } from '@components/Button';
 
 import { Container } from './styles'
 
-export default function Groups() {
+export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>

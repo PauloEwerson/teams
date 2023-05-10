fc0d255c5f96046dabe622c31d7f203eef187bba
PauloEwerson <ewersonmrt@gmail.com>
2023-05-09 21:22:54 -0300

    feat: Utiliza o useNavigation para navegar ao início da pilha

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Container, Content, Icon } from './styles';
+
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Hightlight';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function NewGroup() {
+
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -23,6 +33,7 @@ export function NewGroup() {
         <Button
           title="Criar turma"
           style={{ marginTop: 20 }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
